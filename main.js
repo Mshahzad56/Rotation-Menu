5c31f9e0bd1cf8daaf2f44f58df0dc203769f898
Malik Shahzad <114597689+Mshahzad56@users.noreply.github.com>
2023-07-18 05:25:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const search = document.querySelector('.search');
+    const btn = document.querySelector('.btn');
+    const input = document.querySelector('.input');
+  
+    btn.addEventListener('click', () => {
+      search.classList.toggle('active');
+      input.focus();
+    });
+  });
+  
